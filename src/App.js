7cb770ddd79cb4990ee7a67e04772e64cc58c6e6
itacademyreact <luis.vicente@copernic.tech>
2021-04-20 11:52:30 +0200

    ejercicio 2

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
         <p>
           Sprint 6 de IT Academy
         </p>
+        <p>
+          Este es el ejercicio 2 del sprint 6
+        </p>
       </header>
     </div>
   );
